755d137ba7a1413e28fad617539845a019b3ce95
ankit <sahuankit.murhu@gmail.com>
2025-03-26 10:39:08 +0530

    DB setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,42 @@
 import express from "express";
 import cors from "cors";
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
 const PORT = 4000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-})
+// PostgreSQL connection setup
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER, 
+  host: process.env.POSTGRES_HOST, 
+  database: process.env.POSTGRES_DB, 
+  password: process.env.POSTGRES_PASSWORD, 
+  port: Number(process.env.POSTGRES_PORT), 
+});
+
+// Test the database connection
+pool.connect((err) => {
+  if (err) {
+    console.error("Error connecting to PostgreSQL:", err);
+  } else {
+    console.log("Connected to PostgreSQL");
+  }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM airports");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching tables:", error);
+    res.status(500).send("Error fetching tables");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
